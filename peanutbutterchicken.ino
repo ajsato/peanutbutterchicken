fb38651ed28b15389b77d7e5aed1d927756b3cea
ajsato <themusicalalien@gmail.com>
2017-11-12 12:02:02 -0600

    final

--- peanutbutterchicken.ino
+++ peanutbutterchicken.ino
@@ -8,38 +8,54 @@ const int buttonPin = 2;
 const int yesPin = 3;
 const int noPin = 4;
 const int ledPin = 5;
-//int touchPin = 7;
+const int led2Pin = 6;
+const int vibrationPin = 7;
+const int touchPin = 8;
 
 const int rotaryPin = 0;
-const int tempPin = 1;
+//const int tempPin = 1;
 const int soundPin = 2;
 
 int colorR = 255;
 int colorG = 255;
 int colorB = 255;
 
+int favIndex = 0;
+int favCurr = 0;
+int favR = 0;
+int favG = 0;
+int favB = 0;
+
+bool redSet = false;
+bool greenSet = false;
+bool blueSet = false;
+
 bool runOnlyOnce = true;
+bool runOnlyOnce2 = true;
+int stressCount = 0;
+bool stressCounter50 = true;
+bool stressCounter100 = true;
+bool stressCounter150 = true;
+bool stressCounter200 = true;
 
 void setup() {
-
   pinMode(buttonPin, INPUT); // set mode for the pin (INPUT/OUTPUT)
   pinMode(yesPin, INPUT);
   pinMode(noPin, INPUT);
   pinMode(ledPin, OUTPUT);
-  pinMode(buzzPin, OUTPUT);
+  pinMode(led2Pin, OUTPUT);
+  pinMode(touchPin, INPUT);
 
   pinMode(rotaryPin, INPUT);
-  pinMode(tempPin, INPUT);
+  //pinMode(tempPin, INPUT);
   pinMode(soundPin, INPUT);
+
   lcd.begin(16, 2);
-  Serial.begin(9600);
-
-
 }
 
 void loop() {
   //Introduction
-  /*if (runOnlyOnce) {
+  if (runOnlyOnce) {
     lcd.setCursor(0, 0);
     lcd.print("Greetings, my");
     lcd.setCursor(0, 1);
@@ -65,10 +81,22 @@ void loop() {
     delay(2000);
     clearLcd();
     lcd.setCursor(0, 0);
-    lcd.print("I am your stress");
+    lcd.print("Before we begin,");
     lcd.setCursor(0, 1);
-    lcd.print("assistant.");
+    lcd.print("let's choose a");
     delay(2000);
+    clearLcd();
+    lcd.setCursor(0, 0);
+    lcd.print("color that");
+    lcd.setCursor(0, 1);
+    lcd.print("relaxes you.");
+    delay(2000);
+
+    // chooses color based on rotary and button input
+    while (!(redSet && greenSet && blueSet)) {
+      setIndex_M();
+    }
+
     clearLcd();
     lcd.setCursor(0, 0);
     lcd.print("I will give you");
@@ -88,20 +116,30 @@ void loop() {
     lcd.print("screen flash,");
     delay(2000);
     clearLcd();
+    lcd.setRGB(favR, favG, favB);
+    delay(100);
+    lcd.setRGB(0, 0, 0);
+    delay(10);
+    lcd.setRGB(colorR, colorG, colorB);
     lcd.setCursor(0, 0);
-    lcd.print("please press the");
+    lcd.print("please press any");
     lcd.setCursor(0, 1);
     lcd.print("button to begin.");
     delay(2000);
+    while (!pressAnyButtonButResetAndPower()) {
+
+    }
     clearLcd();
     lcd.setCursor(0, 0);
     lcd.print("Let's try it.");
     delay(2000);
 
     runOnlyOnce = false;
-    } */
+  }
 
   //Operation
+
+  //button
   if (digitalRead(buttonPin)) {
     turnRed();
     if (colorG == 0 || colorB == 0) {
@@ -112,15 +150,14 @@ void loop() {
     turnWhite();
   }
 
-  //Have to check yes no if it works correctly or not.
+  //Sound
+  aggressiveSound();
+  lcd.setCursor(0, 0);
+  lcd.print("Stress Counter: ");
+  lcd.setCursor(0, 1);
+  lcd.print(stressCount);
 
-  //ROTARY
-  //if (analogRead(rotaryPin) != 0) {
-  //  lcd.setCursor(0, 1);
-  //  lcd.print(analogRead(rotaryPin));
-  //  delay(1);
-  //  lcd.print("     ");
-  //}
+  //Have to check yes no if it works correctly or not.
 
   //TEMP SENSOR
   //  lcd.setCursor(0, 1);
@@ -128,31 +165,26 @@ void loop() {
   //  delay(1);
   //  lcd.print("     ");
 
-  //BUZZ
-  //digitalWrite(buzzPin, HIGH);
-  //delay(1);
-  //  digitalWrite(buzzPin, LOW);
-  //  delay(1);
+  //TOUCH SENSOR
+  while (digitalRead(touchPin))
+  {
+    digitalWrite(led2Pin, HIGH);
+    clearLcd();
+    lcd.setCursor(0,0);
+    lcd.print("Relax~");
+    lcd.setRGB(255,255,255);
+    delay(1000);
+    lcd.setRGB(favR, favG, favB);
+  }
 
+  //VIBRATION SENSOR
+  earthquake();
 
-  //problem...
-  //  lcd.setCursor(0, 0);
-  //  lcd.print(analogRead(soundPin));
-  //  delay(100);
-  //  lcd.setCursor(0, 0);
-  //  lcd.print("     ");
+  //EASTER EGGS
+  if (stressCount != 0)
+    easterEgg1();
 
   lcd.setRGB(colorR, colorG, colorB);
-  int sensorVal = analogRead(tempPin);
-  Serial.print("sensor Value: ");
-  Serial.print(sensorVal);
-  float voltage = (sensorVal / 1024.0) * 5.0;
-  Serial.print(", Volts: ");
-  Serial.print(voltage);
-  Serial.print(", degrees C: ");
-  float temperature = (voltage - .5) * 100;
-  Serial.println(temperature);
-
 }
 
 //======================================================================
@@ -190,6 +222,7 @@ void alert()
   digitalWrite(ledPin, HIGH);
   delay(100);
   digitalWrite(ledPin, LOW);
+  stressCount++;
 }
 //======================================================================
 
@@ -206,6 +239,8 @@ bool isYes()
 }
 //======================================================================
 
+//======================================================================
+//press any button
 bool pressAnyButtonButResetAndPower()
 {
   if (digitalRead(yesPin) || (digitalRead(noPin) || digitalRead(buttonPin))) {
@@ -213,11 +248,253 @@ bool pressAnyButtonButResetAndPower()
   }
   return false;
 }
+//======================================================================
 
+//======================================================================
+//clear the LCD screen
 void clearLcd()
 {
   lcd.setCursor(0, 1);
   lcd.print("                ");
   lcd.setCursor(0, 0);
   lcd.print("                ");
+}
+//======================================================================
+
+/*
+  float calculateTemp()
+  {
+  float voltage = (analogRead(tempPin) / 1024.0) * 5.0;
+  float temperature = (voltage - .5) * 100;
+  return temperature;
+  }
+*/
+//======================================================================
+//Detect stress from sound
+void aggressiveSound()
+{
+  if (analogRead(soundPin) >= 900)
+  {
+    alert();
+    colorG = 0;
+    colorB = 0;
+  }
+}
+//======================================================================
+
+//======================================================================
+//flash LED
+void blinkLed(int x)
+{
+  digitalWrite(x, HIGH);
+  delay(100);
+  digitalWrite(x, LOW);
+}
+//======================================================================
+
+//======================================================================
+//measure vibration
+void earthquake()
+{
+  if (digitalRead(vibrationPin))
+  {
+    alert();
+    colorG = 0;
+    colorB = 0;
+  }
+}
+
+//======================================================================
+//It is a secret! Keep quiet!
+void easterEgg1()
+{
+
+  if (stressCount == 50 && stressCounter50)
+  {
+    clearLcd();
+    lcd.setCursor(0, 0);
+    lcd.print("Hooray! You are");
+    lcd.setCursor(0, 1);
+    lcd.print("a stress newbie");
+    delay(2000);
+    clearLcd();
+    stressCounter50 = false;
+  }
+  else if (stressCount == 100 && stressCounter100)
+  {
+    clearLcd();
+    lcd.setCursor(0, 0);
+    lcd.print("Wow! You are a");
+    lcd.setCursor(0, 1);
+    lcd.print("stress amateur!");
+    delay(2000);
+    clearLcd();
+    stressCounter100 = false;
+  }
+  else if (stressCount == 150 && stressCounter150)
+  {
+    clearLcd();
+    lcd.setCursor(0, 0);
+    lcd.print("OMG! You are a ");
+    lcd.setCursor(0, 1);
+    lcd.print("stress master!");
+    delay(2000);
+    clearLcd();
+    stressCounter150 = false;
+  }
+  else if (stressCount == 200 && stressCounter200)
+  {
+    clearLcd();
+    lcd.setCursor(0, 0);
+    lcd.print("...You must have");
+    lcd.setCursor(0, 1);
+    lcd.print("a tough life");
+    delay(2000);
+    clearLcd();
+    lcd.setCursor(0, 0);
+    lcd.print("    (@_____@)   ");
+    clearLcd();
+    stressCounter200 = false;
+  }
+}
+//======================================================================
+
+//======================================================================
+//ROTARY
+float rotary() {
+  clearLcd();
+  lcd.setCursor(0, 1);
+  lcd.print(floor(analogRead(rotaryPin) / 4));
+  delay(1);
+  lcd.print("     ");
+  return  floor(analogRead(rotaryPin) / 4);
+}
+
+//======================================================================
+
+
+//======================================================================
+// left, yes, right
+void setIndex() {
+
+  delay(100);
+  clearLcd();
+
+  // right button is right cursor
+  if (digitalRead(buttonPin)) {
+    switch (favIndex) {
+      case 0:
+      case 1:
+        favIndex++;
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  // left button is left cursor
+  if (digitalRead(noPin)) {
+    switch (favIndex) {
+      case 1:
+      case 2:
+        favIndex--;
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  // middle button is choose
+  if (digitalRead(yesPin)) {
+    switch (favIndex) {
+      case 0:
+        favCurr = rotary();
+        favR = favCurr;
+        lcd.setRGB(favR, favG, favB);
+        lcd.setCursor(0, 0);
+        lcd.print("Red Set");
+        redSet = true;
+        break;
+
+      case 1:
+        favCurr = rotary();
+        favG = favCurr;
+        lcd.setRGB(favR, favG, favB);
+        lcd.setCursor(0, 0);
+        lcd.print("Green Set");
+        greenSet = true;
+        break;
+
+      case 2:
+        favCurr = rotary();
+        favB = favCurr;
+        lcd.setRGB(favR, favG, favB);
+        lcd.setCursor(0, 0);
+        lcd.print("Blue Set");
+        blueSet = true;
+        break;
+
+      default:
+        break;
+    }
+  }
+  // display final color
+  lcd.setRGB(favR, favG, favB);
+}
+//--------------------------------------------------------------------------------------------------------------------------------------
+void setIndex_M() {
+
+  if (favIndex == 0) { //Red state
+    lcd.setCursor(0, 0);
+    lcd.print("Red");
+    favCurr = rotary();
+    lcd.setRGB(favCurr, favG, favB);
+    if (digitalRead(buttonPin)) {
+      favIndex = 1;
+      delay(100);
+    }
+    else if (digitalRead(yesPin)) {
+      favR = favCurr;
+      redSet = true;
+      delay(100);
+    }
+
+
+  }
+  if (favIndex == 1) { //Green state
+    lcd.setCursor(0, 0);
+    lcd.print("Green");
+    favCurr = rotary();
+    lcd.setRGB(favR, favCurr, favB);
+    if (digitalRead(buttonPin)) {
+      favIndex = 2;
+      delay(100);
+    }
+    else if (digitalRead(yesPin)) {
+      favG = favCurr;
+      greenSet = true;
+      delay(100);
+    }
+    else if (digitalRead(noPin)) {
+      favIndex = 0;
+      delay(100);
+    }
+  }
+  if (favIndex == 2) { //Blue state
+    lcd.setCursor(0, 0);
+    lcd.print("Blue");
+    favCurr = rotary();
+    lcd.setRGB(favR, favG, favCurr);
+    if (digitalRead(yesPin)) {
+      favB = favCurr;
+      blueSet = true;
+      delay(100);
+    }
+    else if (digitalRead(noPin)) {
+      favIndex = 1;
+      delay(100);
+    }
+  }
 }
