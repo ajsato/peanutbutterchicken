b59d832cc968918e18499b1ba5b136846f1afc84
Anapat Chairithinugull <chairithinug@wisc.edu>
2017-11-12 04:13:20 -0600

    rotary function dialogue button press

--- peanutbutterchicken.ino
+++ peanutbutterchicken.ino
@@ -8,42 +8,146 @@ int buttonPin = 2;
 int yesPin = 3;
 int noPin = 4;
 int ledPin = 5;
+//int touchPin = 7;
+int buzzPin = 8;
+
+int rotaryPin = 0;
+int tempPin = 1;
+int soundPin = 2;
+
 int colorR = 255;
 int colorG = 255;
 int colorB = 255;
 
 char answer = 'w';
+bool runOnlyOnce = true;
 
 void setup() {
 
   pinMode(buttonPin, INPUT); // set mode for the pin (INPUT/OUTPUT)
+  pinMode(yesPin, INPUT);
+  pinMode(noPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buzzPin, OUTPUT);
+
+  pinMode(rotaryPin, INPUT);
+  pinMode(tempPin, INPUT);
+  pinMode(soundPin, INPUT);
   lcd.begin(16, 2);
-  lcd.print(""); // print to LCD
+
 
 }
 
 void loop() {
+  //Introduction
+  /*if (runOnlyOnce) {
+    lcd.setCursor(0, 0);
+    lcd.print("Greetings, my");
+    lcd.setCursor(0, 1);
+    lcd.print("name is Hans.");
+    delay(3000);
+    clearLcd();
+    lcd.setCursor(0, 0);
+    lcd.print("    (^___^)");
+    delay(2000);
+    clearLcd();
+    lcd.setCursor(0, 0);
+    lcd.print("Press any button");
+    lcd.setCursor(0, 1);
+    lcd.print("to continue.");
+    while (!pressAnyButtonButResetAndPower()) {
 
-  //======================================================================
+    }
+    clearLcd();
+    lcd.setCursor(0, 0);
+    lcd.print("I am your stress");
+    lcd.setCursor(0, 1);
+    lcd.print("assistant.");
+    delay(2000);
+    clearLcd();
+    lcd.setCursor(0, 0);
+    lcd.print("I am your stress");
+    lcd.setCursor(0, 1);
+    lcd.print("assistant.");
+    delay(2000);
+    clearLcd();
+    lcd.setCursor(0, 0);
+    lcd.print("I will give you");
+    lcd.setCursor(0, 1);
+    lcd.print("activities");
+    delay(2000);
+    clearLcd();
+    lcd.setCursor(0, 0);
+    lcd.print("that relieve");
+    lcd.setCursor(0, 1);
+    lcd.print("stress.");
+    delay(2000);
+    clearLcd();
+    lcd.setCursor(0, 0);
+    lcd.print("When you see the");
+    lcd.setCursor(0, 1);
+    lcd.print("screen flash,");
+    delay(2000);
+    clearLcd();
+    lcd.setCursor(0, 0);
+    lcd.print("please press the");
+    lcd.setCursor(0, 1);
+    lcd.print("button to begin.");
+    delay(2000);
+    clearLcd();
+    lcd.setCursor(0, 0);
+    lcd.print("Let's try it.");
+    delay(2000);
+
+    runOnlyOnce = false;
+    } */
+
+  //Operation
   if (digitalRead(buttonPin)) {
     turnRed();
+    if (colorG == 0 || colorB == 0) {
+      alert();
+    }
   }
   else {
     turnWhite();
   }
-  lcd.setRGB(colorR, colorG, colorB);
-  //======================================================================
 
-  // while waiting for user to push button
-  if (answer != ('y' || 'n')) {
-    if (answer == 'y') // if yes
-      lcd.print("y"); 
-    else // if no
-      lcd.print("n"); 
-  }
+  //Have to check yes no if it works correctly or not.
+
+  //ROTARY
+    if (analogRead(rotaryPin) != 0) {
+    lcd.setCursor(0, 1);
+    lcd.print(analogRead(rotaryPin));
+    delay(1);
+    lcd.print("     ");
+    }
+
+  //TEMP SENSOR
+  //  lcd.setCursor(0, 1);
+  //  lcd.print(analogRead(tempPin));
+  //  delay(1);
+  //  lcd.print("     ");
+
+  //BUZZ
+  //digitalWrite(buzzPin, HIGH);
+  //delay(1);
+  //  digitalWrite(buzzPin, LOW);
+  //  delay(1);
+
+
+//problem...
+//  lcd.setCursor(0, 0);
+//  lcd.print(analogRead(soundPin));
+//  delay(100);
+//  lcd.setCursor(0, 0);
+//  lcd.print("     ");
+
+  lcd.setRGB(colorR, colorG, colorB);
 
 }
 
+//======================================================================
 //if button is pressed, the screen turns red.
 void turnRed()
 {
@@ -53,9 +157,11 @@ void turnRed()
   if (colorB > 0) {
     colorB--;
   }
-  delay(40);
+  delay(50);
 }
+//======================================================================
 
+//======================================================================
 //function restore the screen to white light.
 void turnWhite()
 {
@@ -65,23 +171,65 @@ void turnWhite()
   if (colorB < 255) {
     colorB++;
   }
-  delay(30);
+  delay(100);
 }
+//======================================================================
 
+//======================================================================
+//Blink RED LED
 void alert()
 {
   digitalWrite(ledPin, HIGH);
-  delay(50);
+  delay(100);
   digitalWrite(ledPin, LOW);
 }
+//======================================================================
 
-void isYes()
+//======================================================================
+bool isYes()
 {
   while (!(digitalRead(yesPin) || digitalRead(noPin))) {
     if (digitalRead(yesPin))
-      answer = 'y';
+      return true;
     if (digitalRead(noPin))
-      answer = 'n';
+      return false;
   }
+  return false;
 }
+//======================================================================
+
+bool pressAnyButtonButResetAndPower()
+{
+  if (digitalRead(yesPin) || (digitalRead(noPin) || digitalRead(buttonPin))) {
+    return true;
+  }
+  return false;
+}
+
+void clearLcd()
+{
+  lcd.setCursor(0, 1);
+  lcd.print("                ");
+  lcd.setCursor(0, 0);
+  lcd.print("                ");
+}
+
+
+//int readTxt()
+//{
+// FILE *ptr_file;
+// int x;
+
+//ptr_file =fopen("output.txt", "w");
+
+//if (!ptr_file)
+//      return 1;
+
+//for (x=1; x<=10; x++)
+//      fprintf(ptr_file,"%d\n", x);
+
+//    fclose(ptr_file);
+
+//  return  0;
+//}
 
